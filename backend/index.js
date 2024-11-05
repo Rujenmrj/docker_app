0351ea6fb49ab2224fa4673cf9ed31c081132a9a
rujenmrj <rujenmrj15@gmail.com>
2024-11-05 15:58:46 +0545

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');  // Import the CORS package
+
+const app = express();
+const PORT = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Here is some data', data: [1, 2, 3, 4,5,6,7,8,9,10] });
+});
+
+app.listen(PORT,() => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+app.get('/api/product/bird', (req, res) => {
+  res.json({ img: 'https://image.rujenm.com.np/placehold.png',name: 'Bird', price: 1000, description:"description place holder"});
+})
